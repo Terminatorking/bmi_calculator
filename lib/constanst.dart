9a49b3361250e566ca902aa5436a7b7c7b261086
soheil <soheilghazimoradi@gmail.com>
2023-07-11 06:46:53 +0330

    write color constants

---
+++ lib/constanst.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+  static Color scaffoldColor = Colors.black;
+  static Color textColor = Colors.white;
+  static Color colorOfContainer = const Color.fromARGB(255, 31, 31, 51);
+  static Color unSelectColor = const Color.fromARGB(255, 141, 142, 152);
+  static Color red = const Color.fromARGB(255, 235, 21, 85);
+}
